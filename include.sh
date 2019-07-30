01830de5f84f620c0f4f415824484fe6fee3efba
pascal <pascal.morin@codeenigma.com>
2019-07-30 16:38:50 +0200

    Don't assume shell only

--- include.sh
+++ include.sh
@@ -3,8 +3,7 @@
 # Perform actual hooks.
 # @param hook-name
 performHook(){
-  GIT_DIR=$(git rev-parse --show-toplevel 2> /dev/null)
   for file in $(find "$GIT_DIR/.git/hooks/$1.d" -name "*.hook" -print -quit); do 
-    . "$file" "$@";
+    /bin/sh "$file" "$@";
   done
 }
